2862f4fb407a8b7f9a136e942fd87cb6d32ce602
Manish Kumar Singh <manishkumarsingh@Manishs-MacBook-Air-3.local>
2025-07-12 12:53:30 +0530

    second commit

--- src/routes/problemCreator.js
+++ src/routes/problemCreator.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const adminMiddleware = require("../middlewares/adminMiddleware")
+const userMiddleware = require("../middlewares/userMiddleware")
 const problemRouter = express.Router()
-const createProblem = require('../controllers/userProblem')
+const {createProblem,updateProblem,deleteProblem,getProblemById,getAllProblem} = require('../controllers/userProblem')
+const { get } = require('mongoose')
 problemRouter.post("/create",adminMiddleware,createProblem)
-// problemRouter.patch("/:id",problemUpdate)
-// problemRouter.delete("/:id",problemDelete)
+problemRouter.put("/update/:id",adminMiddleware,updateProblem)
+problemRouter.delete("/delete/:id",adminMiddleware,deleteProblem)
 
-// problemRouter.get("/:id",problemFetch)
-// problemRouter.get("/", getAllProblem);
+problemRouter.get("/problemById/:id",userMiddleware,getProblemById)
+problemRouter.get("/getAllProblem",userMiddleware, getAllProblem);
 // problemRouter.get("/user", solvedProblem);
 
 module.exports = problemRouter
